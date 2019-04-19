e5b1ae02073cdcf4b8d339a4a5e48e364bd5be67
Lucas de Souza <lucas.souza@c6bank.com>
2019-04-19 11:11:33 -0300

    First Commit

---
+++ concourse-vault-init.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+docker-compose up -d vault
+
+sleep 1
+
+VAULT_CACERT=${PWD}/concourse-vault/vault-certs/vault-ca.crt
+VAULT_ADDR=https://127.0.0.1:8200
+
+vault operator init -format json &> ${PWD}/concourse-vault/vault-init.json
+
+echo "Vault started"
+
+vault operator unseal $(jq -r .unseal_keys_b64[0] ${PWD}/concourse-vault/vault-init.json)
+vault operator unseal $(jq -r .unseal_keys_b64[1] ${PWD}/concourse-vault/vault-init.json)
+vault operator unseal $(jq -r .unseal_keys_b64[2] ${PWD}/concourse-vault/vault-init.json)
+
+vault login $(jq -r .root_token ${PWD}/concourse-vault/vault-init.json)
+
+vault secrets enable -version=1 -path=concourse kv
+vault policy write concourse ${PWD}/concourse-vault/concourse-policy.hcl
+
+vault token create -format json --policy concourse --period 1h &> ${PWD}/concourse-vault/concourse-token.json
+
+CONCOURSE_TOKEN=$(jq -r .auth.client_token ${PWD}/concourse-vault/concourse-token.json)
+
+sed -i '' -E 's/(CONCOURSE_VAULT_CLIENT_TOKEN=)(.*$)/\1'"${CONCOURSE_TOKEN}"'/g' docker-compose.yml
+
+docker-compose up -d
+
+export VAULT_CACERT
+export VAULT_ADDR
