70aea8fe3aad7b47b135c68f3e6e1e8d3974327e
Lucas de Souza <201293@gmail.com>
2019-05-01 15:03:09 -0300

    Add a pipeline example

--- README.md
+++ README.md
@@ -4,3 +4,9 @@ A way to quick start working with concourse + vault as Credential Manager
 ``` bash
 source concourse-vault-init.sh
 ```
+
+vault write concourse/main/docker-hub-username value='registry:5000'
+
+cd pipeline-example/
+fly -t tutorial sp -p node-linter -c docker-image-linter.yml
+fly -t tutorial up -p node-linter
--- concourse-vault-init.sh
+++ concourse-vault-init.sh
@@ -3,7 +3,6 @@
 
 docker-compose up -d vault
 
-sleep 1
 
 #VAULT_CACERT=${PWD}/concourse-vault/vault-certs/vault-ca.crt
 VAULT_ADDR=http://127.0.0.1:8200
@@ -11,6 +10,7 @@ VAULT_ADDR=http://127.0.0.1:8200
 #vault operator init -format json &> ${PWD}/concourse-vault/vault-init.json
 
 echo "Vault started"
+sleep 5
 
 #vault operator unseal $(jq -r .unseal_keys_b64[0] ${PWD}/concourse-vault/vault-init.json)
 #vault operator unseal $(jq -r .unseal_keys_b64[1] ${PWD}/concourse-vault/vault-init.json)
@@ -23,6 +23,8 @@ vault secrets enable -version=1 -path=concourse kv
 #vault policy write concourse ${PWD}/concourse-vault/concourse-policy.hcl
 
 #vault token create -format json --policy concourse --period 1h &> ${PWD}/concourse-vault/concourse-token.json
+
+vault write concourse/main/docker-hub-username value='registry:5000'
 
 #CONCOURSE_TOKEN=$(jq -r .auth.client_token ${PWD}/concourse-vault/concourse-token.json)
 
